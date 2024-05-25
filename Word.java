709c3d70583bb17a441cb395d8f26f2047b6141f
aidencachia <147393490+aidencachia@users.noreply.github.com>
2024-05-25 23:15:25 +0200

    Added Testing Method

--- Word.java
+++ Word.java
@@ -3,16 +3,16 @@ import java.util.*;
 import java.util.Map.Entry;
 
 class Word{
-    Set<Character> grey;
-    Map<Character,List<Integer>> yellows;
-    Map<Character, Integer> greens;
+    private Set<Character> grey;
+    private Map<Character,List<Integer>> yellows;
+    private List<Character> greens;
 
     public static ArrayList<String> words;
     {
         words = new ArrayList<String>();
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader("WordSets/wordle.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("WordSets/wordleU.txt"));
             while (true) {
                 String line = br.readLine();
                 if(line == null)
@@ -31,7 +31,21 @@ class Word{
     public Word(){
         grey = new HashSet<Character>();
         yellows = new HashMap<>(5);
-        greens = new HashMap<>(5);
+        greens = new ArrayList<>(Collections.nCopies(5, null));
+    }
+
+    public void addGrey(char c){
+        if(!greens.contains(c) && !yellows.containsKey(c))
+            grey.add(c);
+    }
+
+    public void addYellow(char c, int pos){
+        yellows.putIfAbsent(c, new ArrayList<Integer>());
+        yellows.get(c).add(pos);
+    }
+
+    public void addGreen(char c, int pos){
+        greens.set(pos, c);
     }
 
     private boolean grayCheck(String word){
@@ -44,18 +58,22 @@ class Word{
     }
 
     private boolean yellowCheck(String word){
+
         for(Entry<Character, List<Integer>> entry: yellows.entrySet()){
+
             char key = entry.getKey();
             List<Integer> value = entry.getValue();
             boolean charFound = false;
 
             for (int i = 0; i < 5; i++) {
+
                 if (word.charAt(i) == key) {
                     charFound = true;
 
                     if (value.contains(i)) {
                         return false;
                     }
+
                 }
             }
 
@@ -66,28 +84,30 @@ class Word{
     }
 
     private boolean greenCheck(String word){
-        for (int i = 0; i < 5; i++)
-            if (i != greens.getOrDefault(word.charAt(i), i))
-                return false;
 
+        for(int i = 0; i < 5; i++){
+            char val =  greens.get(i) == null ? ' ' : greens.get(i);
+
+            if(word.charAt(i) != val && val != ' '){
+                return false;
+            }
+        }
         return true;
     }
+    int charCount[];
+    int charCountPos[][];
 
-
-    String wordCheck(boolean debug){
-        int charCount[] = new int[26];
-        int charCountPos[][] = new int[26][5];
+    String wordCheck(){
+        charCount = new int[26];
+        charCountPos = new int[26][5];
 
         ArrayList<String> prevWords = words;
         words = new ArrayList<String>();
         for (String word : prevWords) {
 
-            if (debug)
-                System.out.print("");
-
-
-            if(grayCheck(word) && yellowCheck(word) && greenCheck(word))
+            if(grayCheck(word) && yellowCheck(word) && greenCheck(word)){
                 words.add(word);
+            }
             
         }
 
@@ -112,7 +132,70 @@ class Word{
                         check = false;
                 }
                 if (check) {
-                    wordScore += ((charCountPos[currentChar - 97][j])*2.5 + charCount[currentChar -97]);
+                    wordScore += ((charCountPos[currentChar - 97][j])*5 + charCount[currentChar -97]);
+                    prevChars[j] = currentChar;
+                }
+            }
+            if(wordScore > bestWordScore){
+                bestWordScore = wordScore;
+                best = i;
+            }else if (wordScore < worstWordScore) {
+                worstWordScore = wordScore;
+                worst = i;
+            }
+        }
+        return words.get(best);
+    }
+
+
+    String wordCheck(String ans){
+        charCount = new int[26];
+        charCountPos = new int[26][5];
+
+        ArrayList<String> prevWords = words;
+        words = new ArrayList<String>();
+        for (String word : prevWords) {
+
+            if(word.equals(ans)){
+                if(!grayCheck(ans)){
+                    System.err.print("Error: Grey check failed");
+                }
+                if(!yellowCheck(ans)){
+                    System.err.println("Error: Yellow check failed");
+                }
+                if(!greenCheck(ans)){
+                    System.err.println("Error: Green check failed");
+                }
+            }
+
+            if(grayCheck(word) && yellowCheck(word) && greenCheck(word)){
+                words.add(word);
+            }
+            
+        }
+
+        for(String word: words) {
+            for (int i = 0; i < 5; i++) {
+                charCount[word.charAt(i) - 97]++;
+                charCountPos[word.charAt(i) - 97][i]++;
+            }
+        }
+
+        double bestWordScore = Integer.MIN_VALUE;
+        double worstWordScore = Integer.MAX_VALUE;
+
+        for(int i = 0; i < words.size(); i++){
+            double wordScore = 0;
+            char[] prevChars = new char[5];
+            for(int j = 0; j < 5; j++){
+                boolean check = true;
+                char currentChar = words.get(i).charAt(j);
+                for(int k = 0; k < j; k++){
+                    if(prevChars[k] == currentChar)
+                        check = false;
+                }
+                if (check) {
+                    wordScore += ((charCountPos[currentChar - 97][j])*1 + charCount[currentChar -97]);
                     prevChars[j] = currentChar;
                 }
             }
@@ -127,3 +210,5 @@ class Word{
         return words.get(best);
     }
 }
+
+//Coefficients 2,1 = 4.39665
